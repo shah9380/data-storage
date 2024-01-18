237d5ad73ac6ab23f66b3c772d45460a356ad5e0
shah9380 <130676464+shah9380@users.noreply.github.com>
2024-01-18 21:19:42 +0530

    Header created

---
+++ src/components/Header/Header.js
@@ -0,0 +1,8 @@
+const Header = ()=>{
+    return(
+        <header className="w-full text-center bg-blue-500 py-4">
+            <h1 className="text-2xl font-medium">Shah Islam's Directory</h1>
+        </header>
+    )
+}
+export default Header;
